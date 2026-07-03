3077d272724fe4c603c911ba0b025a59ebb35c62
unknown <sucinur0902@gmail.com>
2026-07-03 16:59:00 +0700

    fix api railway

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ HOME ROUTE
 ========================= */
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.send("Backend Running 🚀");
 });
 
 /* =========================
